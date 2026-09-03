27031316da240af1eaf87df1b2090c06a10f5a96
AArek202 <abdelrahaman.amer202@gmail.com>
2026-09-03 16:00:33 +0300

    chore(infra): run two API replicas behind nginx

--- clinic_appointment_booking/src/appointments/appointments.controller.ts
+++ clinic_appointment_booking/src/appointments/appointments.controller.ts
@@ -8,7 +8,7 @@ import {
   ParseUUIDPipe,
   Post,
 } from '@nestjs/common';
-import { AuthUser } from '../auth/auth-user.interface';
+import type { AuthUser } from '../auth/auth-user.interface';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { UserRole } from '../common/enums/role.enum';
